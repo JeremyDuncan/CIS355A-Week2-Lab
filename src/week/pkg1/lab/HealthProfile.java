0aa0de0e13c1542b9274f61e09f8b138e6434a59
Jeremy Duncan <jduncan46@my.devry.edu>
2022-08-29 18:53:41 -0400

    added behaviors, getters and setters

--- src/week/pkg1/lab/HealthProfile.java
+++ src/week/pkg1/lab/HealthProfile.java
@@ -1,34 +1,103 @@
 package week.pkg1.lab;
 
 public class HealthProfile {
-        //attributes
-        private String name;
-        private int age;
-        private double weight;
-        private double height;
+    //attributes
+    private String name;
+    private int age;
+    private double weight;
+    private double height;
 
-        //constructors
+    //constructors
+    public HealthProfile() {
+        name = "unknown";
+        age = 0;
+        weight = 0.0;
+        height = 0.0;
+    }
 
+    public HealthProfile(String name, int age, double weight, double height) {
+        this.setName(name);
+        setAge(age);
+        setWeight(weight);
+        setHeight(height);
+    }
 
+    //behaviors
+    public double calculateBMI() {
+        double bmi = (weight * 703) / (Math.pow(height, 2));
+        return bmi;
+    }
 
-        //behaviors
+    public String calculateCategory() {
+        double bmi = calculateBMI();
+        if (bmi < 18.5)
+            return "Underweight";
+        else if (bmi < 25)
+            return "Normal";
+        else if (bmi < 30)
+            return "Overweight";
+        else
+            return "Obese";
 
+    }
 
-        //getters and setters
-        private void setName(String name) {
-            this.name = name;
-        }
+    public double calculateMaxHR() {
+        return 220 - age;
+    }
 
-        private void setAge(int age) {
+    //getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setName(String newName) {
+        if (newName.length() > 0)
+            this.name = newName;
+        else
+            this.name = "unknown";
+    }
+
+    public void setAge(int age) {
+        if (age > 0)
             this.age = age;
-        }
+        else
+            this.age = 0;
+        this.age = age;
+    }
 
-        private void setWeight(double weight) {
+    public void setWeight(double weight) {
+        if (weight > 0.0)
             this.weight = weight;
-        }
+        else
+            this.weight = 0.0;
+    }
 
-        private void setHeight(double feet, double inches) {
-            this.height = ((feet / 12) + inches);
-        }
+    public void setHeight(double inches) {
+        if (inches > 0.0)
+            this.height = inches;
+        else
+            this.height = 0.0;
+    }
+
+    public void setHeight(int feet, double inches) {
+        double total = (feet * 12) + inches;
+
+        if (total > 0.0)
+            this.height = total;
+        else
+            this.height = 0.0;
+    }
 
 }
